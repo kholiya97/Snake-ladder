1fa3698c3fbb7f3c7b66f9cff104a8d0ef2e558d
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-16 13:24:52 +0530

    Added code of UC3_PLAYER_CHECK_OPTION

--- Program.cs
+++ Program.cs
@@ -4,18 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeAndladder
+namespace SnakeAndLadder
 {
     class Program
     {
+        const int Startposition = 0;    //starting position 0
+        const int NO_PLAY = 1;
+        const int LADDER = 2;
+        const int SNAKE = 3;
 
         static void Main(string[] args)
-        {
+        {  //Entery point
+            Random random = new Random();
+            int option = random.Next(1, 4); //random value genreted 1-3
+            int dieValue = random.Next(1, 7); //random value genreted 1-6
+            Console.WriteLine("Player Roll Dies:" + dieValue);
+            int CurrentPosition = 0;
 
-            Console.WriteLine("Welcome to Game Snake and ladder");
+            switch (option)   //start switch
+            {
+                case NO_PLAY:
+                    Console.WriteLine("No Play");
+                    break;
+                case LADDER:
+                    CurrentPosition = dieValue + CurrentPosition;
+                    Console.WriteLine("You got Ladder \n player position = " + CurrentPosition);
+                    break;
+                case SNAKE:
+                    CurrentPosition = CurrentPosition - dieValue;
+                    Console.WriteLine("You gotSnake \n player position = " + CurrentPosition);
+                    break;
+
+            } //end switch
             Console.ReadLine();
-
-
         }
     }
 }
