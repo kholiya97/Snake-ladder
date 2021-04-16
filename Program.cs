f5992e333a950113c52441b87ea2ed7cc66c2b8d
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-16 13:18:37 +0530

    Added code of UC2_ROLL_A_DICE

--- Program.cs
+++ Program.cs
@@ -4,18 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeAndladder
+namespace SnakeAndLadder
 {
     class Program
     {
-
+        const int position = 0;
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Welcome to Game Snake and ladder");
+            Random random = new Random();
+            int dieValue = random.Next(1, 7);
+            Console.WriteLine(" Player die roll position = " + dieValue);
             Console.ReadLine();
-
-
         }
     }
 }
